e7f64e30d34fc0f281a690e52ebf37a97153ccb9
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-30 13:13:05 +0000

    ValidateUserId middleware created and plugged into get user by id & get user posts by id endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,75 +1,78 @@
-const express = require('express');
-const users = require('./userDb');
+const express = require("express");
+const users = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.post("/:id/posts", (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
-
-});
-
-router.get('/', (req, res) => {
-    users.get()
+router.get("/", (req, res) => {
+  users
+    .get()
     .then(users => {
-        res.status(200).json(users)
+      res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({message: "Error retrieving users: " + err.message})
-    })
+      res
+        .status(500)
+        .json({ message: "Error retrieving users: " + err.message });
+    });
 });
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    users.getById(id)
-    .then(user => {
-        if(user){
-            res.status(200).json(user)
-        } else {
-            res.status(404).json({message: `User with id of ${id} does not exist`})
-        }
-    })
-    .catch(err => {
-        res.status(500).json({message: `Error retrieving user with id of ${id}: ${err.message}`})
-    })
+router.get("/:id", validateUserId, (req, res) => {
+  res.json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-    const { id } = req.params;
-    users.getUserPosts(id)
+router.get("/:id/posts", validateUserId, (req, res) => {
+  users
+    .getUserPosts(req.user.id)
     .then(posts => {
-        if(posts.length){
-             res.status(200).json(posts)
-        } else {
-            res.status(200).json({message: "This user doesn't have any posts"})
-        }
+      if (posts.length) {
+        res.status(200).json(posts);
+      } else {
+        res.status(200).json({ message: "This user doesn't have any posts" });
+      }
     })
     .catch(err => {
-        res.status(500).json({message: `Error retrieving posts for user with id of ${id}: ${err.message}`})
-    })
+      res
+        .status(500)
+        .json({
+          message: `Error retrieving posts for user with id of ${id}: ${err.message}`
+        });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  const { id } = req.params;
+  users
+    .getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({ message: `User with id of ${id} does not exist` });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          message: `Error retrieving user with id of ${id}: ${err.message}`
+        });
+    });
+}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
