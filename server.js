3874aa00f187edf1fdba31312605dda36b808c5b
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-30 12:35:21 +0000

    Logger middleware implemented, runs on every req to API

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const server = express();
 server.use(express.json());
 server.use('/api/users', userRouter);
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`\n***NEW REQUEST*** \nRequest Method: ${req.method} \nRequest URL: ${req.url} \nRequest Timestamp: ${Date.now()}\n`);
+  next();
 };
 
 module.exports = server;
